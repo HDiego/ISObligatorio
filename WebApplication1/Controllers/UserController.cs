0336180bfab87934d29c460a5b4240be09d17060
Diego Rocca <diegorocca15@gmail.com>
2015-11-04 22:47:17 -0300

    Mucho

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Colabora.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult LoginUser(User user)
+        {
+            return View();
+        }
+
+    }
+}
