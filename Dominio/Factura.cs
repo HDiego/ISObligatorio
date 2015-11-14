da00dd77f96c33caebe9b093e338b169c6880b77
Mauricio <maunintendo@gmail.com>
2015-11-14 15:35:46 -0300

    Factura y sala error de git y exclusion de versionado arreglada

---
+++ Dominio/Factura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Factura
+    {
+        public Cliente Cliente { get; set; }
+        public Reserva Reserva { get; set; }
+        public double TotalAPagar { get; set; }
+        public double IVA { get; set; }
+        public bool EsPorReserva { get; set; }
+        public Membresia Membresia { get; set; }
+
+        public Factura(bool esPorReserva)
+        {
+            this.EsPorReserva = esPorReserva;
+        }
+    }
+}
---
+++ Dominio/Sala.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Sala
+    {
+        public string ID { get; set; }
+        public string TipoSala { get; set; }
+        public int CapacidadMaxima { get; set; }
+        public string Descripcion { get; set; }
+
+        public Sala(string id, string tipo, int capacidad, string descripcion)
+        {
+            this.ID = id;
+            this.TipoSala = tipo;
+            this.CapacidadMaxima = capacidad;
+            this.Descripcion = descripcion;
+        }
+
+        public Sala(string id)
+        {
+            this.ID = id;
+        }
+    }
+}
